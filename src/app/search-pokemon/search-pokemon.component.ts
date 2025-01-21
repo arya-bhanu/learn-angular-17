34debdeb61e7a337c150949724b8a97c82608b0c
arya-bhanu <aryacreative.dev@gmail.com>
2025-01-21 11:57:56 +0700

    feat: search pokemon with pipe obs

--- src/app/search-pokemon/search-pokemon.component.ts
+++ src/app/search-pokemon/search-pokemon.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   catchError,
   debounceTime,
@@ -21,7 +21,7 @@ import { SearchPokemonService } from './search-pokemon.service';
 @Component({
   selector: 'app-search-pokemon',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './search-pokemon.component.html',
   styleUrl: './search-pokemon.component.css',
 })
@@ -33,9 +33,35 @@ export class SearchPokemonComponent implements OnInit, OnDestroy {
   error: string | null = null;
   loading = false;
 
+  q = new FormControl('');
+  subsQPokemon!: Subscription;
+  pokemonQ!: Pokemon;
+  pokemonQObs$!: Observable<Pokemon | null>;
+
   constructor(private readonly searchPokemonService: SearchPokemonService) {}
 
   ngOnInit(): void {
+    this.pokemonQObs$ = this.q.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap((search) => {
+        return this.searchPokemonService.searchPokemon(search || '').pipe(
+          catchError((err) => {
+            console.error(err);
+            return of(null);
+          })
+        );
+      }),
+      map((res: any) => {
+        if (res == null) return null;
+        return {
+          name: res.name,
+          base_experience: res.base_experience,
+          height: res.height,
+          id: res.id,
+        } as Pokemon;
+      })
+    );
     this.subsPokemon = this.subjectPokemon$
       .pipe(debounceTime(300), distinctUntilChanged())
       .subscribe((val) => {
@@ -63,6 +89,7 @@ export class SearchPokemonComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subsPokemon.unsubscribe();
+    this.subsQPokemon.unsubscribe();
   }
 
   searchPokemon(name: string) {
