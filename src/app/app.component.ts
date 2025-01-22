cb5e824fae9bcd7a63723842a4e918c76368af82
arya-bhanu <aryacreative.dev@gmail.com>
2025-01-22 14:58:59 +0700

    feat: rxjs-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject, map, Observable, of, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'learn-angular-17';
+  b = new BehaviorSubject<string>('Init');
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  inputForm = this.formBuilder.group({ input: '' });
+  valPipe!: Observable<string>;
+
+  ngOnInit(): void {
+    const a = of(['Hello']);
+    a.subscribe({
+      next: (val) => {
+        console.log(val);
+      },
+    }).unsubscribe();
+
+    this.valPipe = this.b.pipe(map((val) => `Pipe: ${val}`));
+
+    this.b.subscribe({
+      next: (val) => {
+        console.log(val);
+      },
+    });
+  }
+
+  clickSubs() {
+    const s = this.inputForm.value.input || '';
+    this.b.next(s);
+  }
 }
